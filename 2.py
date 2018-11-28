0e6ea4413d386c84f191ea0cf5b851c573b6867f
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-11-28 10:11:23 +0000

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,18 @@
+x = 15
+y = 5
+#Excercise: check if x is bigger that y
+if x>y:
+    print("x is greater that y")
+else:
+    print("x is not greater than y")
+#Boolean condition
+
+#it will give a result of false
+print("x == y", x==y)
+#now will give true
+y=15
+
+print("y == x", x==y)
+print("x>=y", x>=y)
+print("x<=y", x<=y)
+print("x!=y", x!=y)
