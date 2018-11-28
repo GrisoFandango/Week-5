82c53581a8654dd583f839c3b61c546416a9bbc3
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-11-28 10:12:09 +0000

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+x = 15
+y = 15
+
+#Logical operator
+print("x and y is",x and y) #reversing the variable give  different answer
+print("x or y is",x or y)
+print("x is not y is",x is not y)
+# x+=y it mean that x = (x+y)
+
+print(x%y)
