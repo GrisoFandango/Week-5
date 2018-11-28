726dda10a260b738f37119dd473839dcf3ac7256
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-11-28 10:09:10 +0000

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,13 @@
+x = 15
+y = 5
+print("X + Y is equal\t" + str(x + y))
+print("X * Y is equal\t" + str(x * y))
+print("X - Y is equal\t" + str(x - y))
+print("X / Y is equal\t" + str(x / y))
+#if i put a comma i am not concatenating a string to an integer so i
+#do not need to use the str() formula that otherwise would be need in case
+#instead of comma we use +. So, + is a concatenator.  
+print("X + Y is equal\t",x+y)
+print("X * Y is equal\t",x*y)
+print("X / Y is equal\t",x/y)
+print("X - Y is equal\t",x-y)
