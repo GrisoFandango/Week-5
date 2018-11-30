73badfa33d240d74af6834f22f6ca507f46e1f40
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-11-30 18:07:06 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,14 +5,16 @@ if x>y:
     print("x is greater that y")
 else:
     print("x is not greater than y")
-#Boolean condition
 
+#Boolean condition
 #it will give a result of false
 print("x == y", x==y)
+
 #now will give true
 y=15
 
-print("y == x", x==y)
-print("x>=y", x>=y)
-print("x<=y", x<=y)
-print("x!=y", x!=y)
+#checking different conditions
+print("y == x", x==y) #equal
+print("x>=y", x>=y)  # x greater than y
+print("x<=y", x<=y) # x lower than y
+print("x!=y", x!=y) # x different than y
