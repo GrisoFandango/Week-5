a9f7f68400a75cedf9d80e7ce679888574ba743c
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-11-30 18:19:39 +0000

    Update 3.py
    improved the readability of the output

--- 3.py
+++ 3.py
@@ -1,10 +1,16 @@
 x = 15
-y = 15
-
+y = 5
+print("X is",x)
+print("Y is",y) 
 #Logical operator
-print("x and y is",x and y) #reversing the variable give  different answer
+print("x and y is",x and y) #reversing the variables
+print("y and x is",y and x) #give  different answer
 print("x or y is",x or y)
+print("y or x is",y or x)
 print("x is not y is",x is not y)
-# x+=y it mean that x = (x+y)
+print("y is not x is",y is not x)
 
-print(x%y)
+
+x+=y #it mean that x = (x+y)
+print("x += y is",x)
+print("X % Y is",x%y)
